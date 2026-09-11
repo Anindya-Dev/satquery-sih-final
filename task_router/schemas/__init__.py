f155ca5af8e5d206221f1182db6f3ccba921bbc7
Arik-code98 <arik.work.c@gmail.com>
2026-09-12 00:05:58 +0530

    feat(task_router): add strict task specification schemas and pydantic models (Part 1)

---
+++ task_router/schemas/__init__.py
@@ -0,0 +1,17 @@
+from .task_spec_models import (
+    TaskType,
+    ModalityRequirement,
+    SensorType,
+    SpecialistTool,
+    TaskParameters,
+    TaskSpec
+)
+
+__all__ = [
+    "TaskType",
+    "ModalityRequirement",
+    "SensorType",
+    "SpecialistTool",
+    "TaskParameters",
+    "TaskSpec"
+]
